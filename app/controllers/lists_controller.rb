322f811a7f1b621b6aa635f43bf551f77145bd49
KoheiWatanabe <161041321+WatanabeKMF@users.noreply.github.com>
2024-03-17 08:18:31 +0000

    [Finish]3-8 バリデーションの設定

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,13 +15,12 @@ class ListsController < ApplicationController
   end
 
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-   # redirect_to '/top' を削除して、以下コードに変更
-    # 詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -31,11 +30,11 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
-  
+
   def destroy
     list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
-    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
   end
 
   private
